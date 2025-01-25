bda10c6ffcbaa2a910443eb73a7e49853595e391
OpenJ92 <jacob.vartuli.92@gmail.com>
2025-01-25 16:14:19 -0500

    add rts_sanc Source impl. Depc

--- starcraft_gather_manager/sources/rts_sanctuary.py
+++ starcraft_gather_manager/sources/rts_sanctuary.py
@@ -1,1 +1,24 @@
-# https://www.rts-sanctuary.com/StarCraft-2/act=Attach&type=post&id={id}
+from starcraft_gather_manager.typeclass.source import Source
+
+class RTSSanctuary(Source):
+    _instance = None
+
+    def __new__(cls, *args, **kwargs):
+        if cls._instance is None:
+            cls._instance = super(RTSSanctuary, cls).__new__(cls)
+            cls._instance._name = "rts-sanctuary"
+            cls._instance._metadata = {"site_description": "RTS-Sanctuary (inaccessible)"}
+            cls._instance._limit = 0  # No available resources
+            cls._instance._count = 0
+        return cls._instance
+
+    @property
+    def name(self) -> str:
+        return self._name
+
+    @property
+    def metadata(self) -> dict:
+        return self._metadata
+
+    def url_formatter(self, specifier: str) -> str:
+        raise NotImplementedError("RTS-Sanctuary is currently inaccessible.")
